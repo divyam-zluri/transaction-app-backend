8cafec324dbef8c09ffbf0751343f584171ecdb4
divyam-zluri <divyam.s@zluri.com>
2025-01-26 23:46:53 +0530

    some more checks

--- src/services/pageLimit.service.ts
+++ src/services/pageLimit.service.ts
@@ -1,5 +1,8 @@
 export function check(page: string | undefined, limit: string | undefined): boolean {
     if (page === undefined || limit === undefined) {
+        if((page == undefined && isNaN(Number(limit))) || (limit == undefined && isNaN(Number(page)))){
+            return false;
+        }
         return true;
     }
     if (isNaN(Number(page)) || isNaN(Number(limit))) {
